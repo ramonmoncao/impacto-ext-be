1bfaffc28b7876ddc784c5fe71f0b607be9118d7
Ramon Moncao <ramonmoncao@hotmail.com>
2026-03-10 08:17:29 -0300

    initial commit

---
+++ src/main/java/com/fatecpi/impacto_ext/ImpactoExtApplication.java
@@ -0,0 +1,13 @@
+package com.fatecpi.impacto_ext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactoExtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactoExtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatecpi/impacto_ext/ImpactoExtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecpi.impacto_ext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactoExtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
