aa18f06c58b235efcbfba35314a4779a39ea9030
Erik Gomes <gomeserik35@gmail.com>
2026-05-27 22:27:15 -0300

    autorização para rota do pdf

--- src/main/java/com/fatecpi/impacto_ext/security/SecurityConfig.java
+++ src/main/java/com/fatecpi/impacto_ext/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .requestMatchers("/users/register", "/users/login", "/api/auth/login").permitAll()
+                .requestMatchers("/users/register", "/users/login", "/api/auth/login", "/api/orcamentos/*/pdf").permitAll()
                 .requestMatchers("/clientes/**").authenticated()
                 .anyRequest().authenticated())
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
